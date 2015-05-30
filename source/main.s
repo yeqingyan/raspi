b0dbff6675ef6f0d9d9821ff68d032e9996e9adf
AyWizard <aywizard@gmail.com>
2015-05-29 23:33:13 -0700

    Lesson2 OK02

--- source/main.s
+++ source/main.s
@@ -3,7 +3,7 @@
  * Details in GCC Internals documentation(How Initialization Functions Are Handled)
  */
 .section .init
-.globl _start	/* Define global symbol _start, make this symbol visible to ld */
+.globl _start   /* Define global symbol _start, make this symbol visible to ld */
 _start:
 /* 
  * store 0x20200000 into register r0
@@ -11,19 +11,52 @@ _start:
  */
 ldr r0,=0x20200000
 
-mov r1, #1        /* Move 1 into register 1 */
-lsl r1, #18       /* shift value in register 1 left by 18 places */
-
 /*
  * Send value in register r1 to GPIO Controller 
  * Enable 16th GPIO pin
+ * Address: GPIO Function Select 1(GPFSEL1)
+ * Register value: Function Select 16(FSEL16)
  */ 
-str r1, [r0, #4]
+mov r1, #1        
+lsl r1, #18       
+str r1, [r0, #4]        /*0x20200004*/
 
 mov r1, #1
 lsl r1, #16
-str r1, [r0, #40] /* Turn the 16th pin off, the light will on */
-
 loop$:
+/*
+ * Turn off GPIO 16th pin, the light will on
+ * Address: GPIO Pin Output Clear 0(GPCLR0)
+ * Register value: Clean GPIO pin 16
+ */
+str r1, [r0, #40]       /*0x20200028*/
+
+/*
+ * Wait, repeat substract 1 in register 2, until value in register 2 became 0
+ */
+mov r2, #0x3F0000
+
+wait1$:
+        sub r2, #1
+        cmp r2, #0
+        bne wait1$
+
+/*
+ * Turn off the light  
+ * Address: GPIO Pin Output Set 0(GPSET0)
+ * Register value: Set 0 GPIO pin 16
+ */
+str r1, [r0, #28]       /*0x2020001C*/
+
+/*
+ * Wait2 is the same as wait1
+ */
+mov r2, #0x3F0000
+wait2$:
+        sub r2, #1
+        cmp r2, #0
+        bne wait2$
+
+/* Loop over this process*/
 b loop$
 
